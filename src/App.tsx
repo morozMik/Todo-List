5e24a14fc1befb48daa0b81772949821ec9740cd
morozMyk <mik.moroz0387@gmail.com>
2024-07-10 12:09:47 +0300

    added basic html

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
-
-import { useState } from 'react'
 import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Button, Container, Stack } from "react-bootstrap"
 
 function App() {
-  const [newTask, setNewTask] = useState('')
+
 
   return (
     <>
-      <h1>Add new task</h1>
-      <div className='NewTask'>
-        <textarea name="new task" id="new task" ></textarea>
+    <div className='TodoInput1'>
+      <h1>Todo Input</h1>
+        <div className='TodoInput2'>
+          <div className='NewTodo'>new todo</div>
+          <div className='NewTodoDes'>Add description</div>
+          <button className='AddNewTask'>Add new task</button>
+        </div>
+    </div> 
+    <div className='ToDoList'>
+      <h1>ToDo List</h1>
+      <div>
+        <Stack direction='horizontal' gap={4} className='buttonStack'>
+          <Button className='w-100' size='lg'>All</Button>
+          <Button className='w-100' size='lg'>Done</Button>
+          <Button className='w-100' size='lg'>Todo</Button> 
+        </Stack>
       </div>
+    </div>
     </>
   )
 }
